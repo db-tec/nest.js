d82b86373ad3a8c946eb18efcd2c3bce93180ad6
db-tec <eng.davidborges@gmail.com>
2020-01-27 12:26:03 -0300

    fix app.modules.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksController } from './tasks/tasks.controller';
-import { Task.Service } from './tasks/shared/task.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TasksModule],
-  controllers: [AppController, TasksController],
-  providers: [AppService, Task.Service],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {}
+
