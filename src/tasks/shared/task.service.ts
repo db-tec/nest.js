ec4fece497bcfdb90416838929a2081b51c9ecbe
db-tec <eng.davidborges@gmail.com>
2020-01-27 14:46:41 -0300

    novo branch homologacao

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    tasks: Task[] = [
+        {id: 1, description: 'tarefa 1', completed: false},
+        {id: 2, description: 'tarefa 2', completed: false},
+        {id: 3, description: 'tarefa 3', completed: true},
+        {id: 4, description: 'tarefa 4', completed: false},
+        {id: 5, description: 'tarefa 5', completed: false},
+        {id: 6, description: 'tarefa 6', completed: false},
+        {id: 7, description: 'tarefa 7', completed: false},
+        {id: 8, description: 'tarefa 8', completed: true},
+        {id: 9, description: 'tarefa 9', completed: false},
+        {id: 10, description: 'tarefa 10', completed: false},        
+    ]; 
+
+    getAll(){
+        return this.tasks;
+
+    }
+
+    getById(id: number){
+        const task = this.tasks.find((value) => value.id == id);
+
+    }
+
+    create(task: Task){
+
+    }
+
+    udate(task: Task){
+
+    }
+
+
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { TaskService } from './shared/task.service';
+import { Task } from './shared/task';
+
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    constructor(
+        private taskService: TaskService
+    ) { }
+
+    @Get()
+    async getAll() : Promise<Task[]> {
+        return this.taskService.getAll();
+    }
+    
+    @Get(':id')
+    async getById(@Param('id') id: number) : Promise<Task>{
+        return this.taskService.getById(id);
+    }
+
+    
+
+}
